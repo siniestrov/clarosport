4490831e3d2075475df3abc5d249855ad33e56c2
siniestrov <siniestro_vt@hotmail.com>
2023-09-29 13:48:26 -0600

    Primera actualizacion

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// Funcion para el contador
+function actualizarContador() {
+    const fechaFinal = new Date('2023-10-30T00:00:00');
+    const ahora = new Date();
+
+    const diferencia = fechaFinal - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    document.getElementById('dias').textContent = dias.toString().padStart(2, '0');
+    document.getElementById('horas').textContent = horas.toString().padStart(2, '0');
+    document.getElementById('minutos').textContent = minutos.toString().padStart(2, '0');
+    document.getElementById('segundos').textContent = segundos.toString().padStart(2, '0');
+}
+
+actualizarContador();
+setInterval(actualizarContador, 1000);
